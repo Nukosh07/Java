77199d6e4e99a07c7500920c4d30f3f7f95353d9
Tuimebai Nursultan <tujmebajnursultan@gmail.com>
2024-10-30 09:56:41 +0500

    example 1

---
+++ exsamplse/js.js
@@ -0,0 +1,16 @@
+var name = "nazar" // ауыспалы айнымалы
+
+name = "Abumansur"
+
+name = "Syrym"
+
+name = "dosik"
+
+const phone = "192.168.123.10"; // туракты 
+
+phone = "192.172.35.10"
+
+console.log(phone) //кате кайтарады
+
+
+document.getElementById("username").innerHTML = name;
