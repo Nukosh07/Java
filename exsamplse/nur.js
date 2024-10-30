db0996f16145d42439f6aa349f91c9b66abb42c7
Tuimebai Nursultan <tujmebajnursultan@gmail.com>
2024-10-30 10:36:47 +0500

    nur

---
+++ exsamplse/nur.js
@@ -0,0 +1,13 @@
+var streetName = "Maulenov, 92"; // string
+var currentDay = 30; //number
+var Amirkeldi = false; // Boolean true = шындык,false = otirik
+var janaSumka = null; // Bos man
+var porshe = undefined; // Jok ainumali
+
+
+
+console.log ("коше", streetName)
+console.log ("Казыргы тун", currentDay)
+console.log ("Амыр келды", Amirkeldi)
+console.log ("Жана сатып алынган сумка ышындегы заттар", janaSumka)
+console.log ("Порше бар", porshe)
