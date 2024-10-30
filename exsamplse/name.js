50e09e99cafde25cd55d5f31da5b95ef68263f0c
Tuimebai Nursultan <tujmebajnursultan@gmail.com>
2024-10-30 10:48:53 +0500

    Озым жайды

---
+++ exsamplse/name.js
@@ -0,0 +1,13 @@
+var name = "Nursultan"
+var surname = "Tuimebai"
+var age = 17;
+var weight = 65; //Salmak
+var height = 1.81; //Boi
+var school = "IT"
+
+console.log("Есымы", name)
+console.log("Тегы", surname)
+console.log("Жасы", age)
+console.log("салмагы", weight)
+console.log("Бойы", height)
+console.log("Оку орны", school)
